c5dd84e0ed57f941670e865ede68f9d6841e8d18
FXavier <francisco.jaramillo01@epn.edu.ec>
2021-11-09 11:31:19 -0500

    Components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,15 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SlidesComponent } from './slides/slides.component';
 import { StartComponent } from './start/start.component';
-import { LogoComponent } from './logo/logo.component';
 import { MorebtnComponent } from './morebtn/morebtn.component';
 
-
-
-
 @NgModule({
-  declarations: [SlidesComponent, StartComponent, LogoComponent, MorebtnComponent],
-  exports: [ SlidesComponent, StartComponent, LogoComponent, MorebtnComponent ],
+  declarations: [SlidesComponent, StartComponent,MorebtnComponent],
+  exports: [ SlidesComponent, StartComponent, MorebtnComponent ],
   imports: [
     CommonModule
   ]
